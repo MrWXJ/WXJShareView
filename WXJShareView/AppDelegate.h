2cc376fc69cc05e597fe45e95ff2e3a36a8fcfe2
MrWXJ <kfb1@guizhouzhongzhiboxindeMacBook-Air.local>
2018-08-04 10:05:23 +0800

    Initial Commit

---
+++ WXJShareView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WXJShareView
+//
+//  Created by MrWXJ on 2018/8/4.
+//  Copyright © 2018年 MrWXJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WXJShareView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WXJShareView
+//
+//  Created by MrWXJ on 2018/8/4.
+//  Copyright © 2018年 MrWXJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
